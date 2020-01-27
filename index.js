2c314b69eaef187a85795fd0b532bc260d0d59d1
fbzr <fabricio.spb@gmail.com>
2020-01-27 11:13:53 -0800

    getName and makeSmartPerson

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return `Hello, my name is ${person.name}`;
 }
 
 /**
@@ -77,8 +77,14 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  return ({
+  	name: name,
+  	sum: ((num1, num2) => {
+  			return num1 + num2;
+  		}),
+  	speak: (() => `Hello, my name is ${name}`)
+  });
 }
 
 
